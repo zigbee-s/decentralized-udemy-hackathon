b712a375be887304a13796bdcca7367c8353d759
Gauraang Sharma <zigbee.sh@gmail.com>
2023-07-01 19:13:59 +0530

    Add template

---
+++ frontend/App.js
@@ -0,0 +1,48 @@
+import 'regenerator-runtime/runtime';
+import React from 'react';
+
+import './assets/global.css';
+
+import { EducationalText, SignInPrompt, SignOutButton } from './ui-components';
+
+
+export default function App({ isSignedIn, contractId, wallet }) {
+  const [valueFromBlockchain, setValueFromBlockchain] = React.useState();
+
+  const [uiPleaseWait, setUiPleaseWait] = React.useState(true);
+
+  // Get blockchian state once on component load
+  React.useEffect(() => {
+    getGreeting()
+      .then(setValueFromBlockchain)
+      .catch(alert)
+      .finally(() => {
+        setUiPleaseWait(false);
+      });
+  }
+    , []);
+
+  /// If user not signed-in with wallet - show prompt
+  if (!isSignedIn) {
+    // Sign-in flow will reload the page later
+    return <SignInPrompt greeting={valueFromBlockchain} onClick={() => wallet.signIn()} />;
+  }
+
+
+  function getGreeting() {
+    // use the wallet to query the contract's greeting
+    return wallet.viewMethod({ method: 'get_greeting', contractId })
+  }
+
+  return (
+    <>
+      <SignOutButton accountId={wallet.accountId} onClick={() => wallet.signOut()} />
+      <main className={uiPleaseWait ? 'please-wait' : ''}>
+        <h1>
+        </h1>
+
+        <EducationalText />
+      </main>
+    </>
+  );
+}
